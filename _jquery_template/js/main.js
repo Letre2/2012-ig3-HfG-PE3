3cca157501e0c4e6816d5f7c5a7cf2eec3f1acf1
Tobias Ertel <64661338+Letre2@users.noreply.github.com>
2020-10-26 18:30:50 +0100

    first class
    fist class

--- _jquery_template/js/main.js
+++ _jquery_template/js/main.js
@@ -1,1 +1,23 @@
-test
+
+$('.number-spinner').mousedown(function(e) {
+  let clickedElement = this;
+  let clickX = e.clientX;
+  let clickY = e.clientY;
+  let clickValue = parseFloat($(clickedElement).text());
+
+  $(window).mousemove(function(e) {
+    let actX = e.clientX;
+    let distX = actX - clickX;
+    let value = $(clickedElement).text();
+
+    let newValue = clickValue + distX;
+
+    $(clickedElement).text(newValue)
+  });
+
+
+  $(window).mouseup(function(e) {
+    $(window).off("mousemove");
+  });
+
+});
